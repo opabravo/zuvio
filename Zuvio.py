c0e6bddf1f4355fe1ebe2df4bf8eed3f3230b634
Your Name <you@example.com>
2021-04-28 13:41:31 +0800

    Fix some issues

--- Zuvio.py
+++ Zuvio.py
@@ -11,6 +11,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.common.exceptions import TimeoutException
 
 chrome_options = webdriver.ChromeOptions()
 
